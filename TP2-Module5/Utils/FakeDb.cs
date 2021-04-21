7364b62b0cd8a4d90453b124dfcd8faab30ebd11
Munegu <nicolas.villetelle@hotmail.fr>
2021-04-21 15:04:25 +0200

    Add TP2-Module

---
+++ TP2-Module5/Utils/FakeDb.cs
@@ -0,0 +1,27 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP2_Module5.Utils
+{
+    public class FakeDb
+    {
+        private static readonly Lazy<FakeDb> lazy = new Lazy<FakeDb>(() => new FakeDb());
+
+        public static FakeDb Instance { get { return lazy.Value; } }
+
+        private FakeDb()
+        {
+            this.InitialiserDatas();
+        }
+
+        public List<Pizza> Pizzas { get; } = new List<Pizza>();
+
+        private List<Pizza> InitialiserDatas()
+        {
+            return Pizzas;
+        }
+    }
+}
