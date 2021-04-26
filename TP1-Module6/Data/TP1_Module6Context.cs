7cf54d1fde7ac626f15c15badb3bb5d9217be804
Munegu <nicolas.villetelle@hotmail.fr>
2021-04-26 15:54:52 +0200

    add CRUD

---
+++ TP1-Module6/Data/TP1_Module6Context.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TP1_Module6.Data
+{
+    public class TP1_Module6Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public TP1_Module6Context() : base("name=TP1_Module6Context")
+        {
+        }
+
+        public System.Data.Entity.DbSet<TP1_Module6.BO.Samourai> Samourais { get; set; }
+
+        public System.Data.Entity.DbSet<TP1_Module6.BO.Arme> Armes { get; set; }
+    }
+}
---
+++ TP1-Module6/Models/SamuraiViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TP1_Module6.BO;
+
+namespace TP1_Module6.Models
+{
+    public class SamuraiViewModel
+    {
+        public Samourai Samurai { get; set; }
+        public List<Arme> Armes { get; set; }
+        public int? IdSelectedArme { get; set; }
+    }
+}
