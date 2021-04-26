dfeea79beff0b9c9dd734ba3cbc27c9946836c77
Munegu <nicolas.villetelle@hotmail.fr>
2021-04-26 11:59:08 +0200

    Add TP1-Module6

---
+++ TP1-Module6/BO/Arme.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP1_Module6.BO
+{
+    public class Arme
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public int Degats { get; set; }
+    }
+}
---
+++ TP1-Module6/BO/Samurai.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP1_Module6.BO
+{
+    public class Samourai
+    {
+        public int Id { get; set; }
+        public int Force { get; set; }
+        public string Nom { get; set; }
+        public virtual Arme Arme { get; set; }
+    }
+}
